14ee72d6526a477283fdf2f5f439e08142094f1a
Felipe Santos <felipe.santos11@hotmail.com>
2024-05-01 18:54:33 -0300

    feat: custom listener

--- str-consumer/src/main/java/com/br/felipe/strconsumer/listeners/StringConsumerListener.java
+++ str-consumer/src/main/java/com/br/felipe/strconsumer/listeners/StringConsumerListener.java
@@ -1,7 +1,8 @@
 package com.br.felipe.strconsumer.listeners;
 
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import com.br.felipe.strconsumer.custom.StringConsumerCustomListener;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -9,8 +10,8 @@ import lombok.extern.log4j.Log4j2;
 @Component
 public class StringConsumerListener {
 		
-	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "stringContainerFactory") 
-	public void listener(String message) {
+	@StringConsumerCustomListener(groupId = "group-1")
+	public void create(String message) {
 		log.info("Receive message {}", message);
 	}
 	
