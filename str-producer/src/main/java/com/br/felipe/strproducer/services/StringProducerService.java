54cc387167ddc8cff37687edc2fcedfe22fdba6a
Felipe Santos <felipe.santos11@hotmail.com>
2024-05-01 16:27:38 -0300

    feat: callback for logs

--- str-producer/src/main/java/com/br/felipe/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/br/felipe/strproducer/services/StringProducerService.java
@@ -5,7 +5,9 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class StringProducerService {
@@ -16,6 +18,16 @@ public class StringProducerService {
 	private String strTopic;
 	
 	public void sendMessage(String message) {
-		kafkaTemplate.send(strTopic, message);
+		kafkaTemplate.send(strTopic, message).addCallback(
+				success -> {
+					if (success != null) {
+						log.info("Send message with success {}", message);
+						log.info("Partition {}, Offset {}", 
+								success.getRecordMetadata().partition(),
+								success.getRecordMetadata().offset());
+					}
+				},
+				error -> log.error("Error send message")
+				);
 	}
 }
